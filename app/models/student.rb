40f080386882152ea901c4622cb71aa47e95e899
Nate <7nater.nelson@gmail.com>
2021-03-22 14:53:23 -0600

    commit tree e55ca4fa3c093a8e0270484def2a1d590702fe69

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,13 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_term)
+    search_term.empty? ? Student.all : Student.where("name LIKE ?", "%#{search_term}%")
+  end
+
+
 end
